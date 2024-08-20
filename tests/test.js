923103e01cc5543897ca25ac5ac08071e61c33a8
Georgy <balabanov.georgy@mail.com>
2024-08-21 02:11:27 +0300

    Initial commit

---
+++ tests/test.js
@@ -0,0 +1,18 @@
+const { test, expect } = require("@playwright/test");
+const { email, password, invalidEmail, invalidPassword } = require("../user.js");
+
+test("Successfull authorization", async ({ page }) => {
+  await page.goto("https://netology.ru/?modal=sign_in");
+  await page.getByPlaceholder("Email").fill(email);
+  await page.getByPlaceholder("Пароль").fill(password);
+  await page.getByTestId("login-submit-btn").click();
+  await expect(page.locator("h2")).toHaveText("Моё обучение", { timeout: 10000,});
+});
+
+test("Unsuccessfull authorization", async ({ page }) => {
+  await page.goto("https://netology.ru/?modal=sign_in");
+  await page.getByPlaceholder("Email").fill(invalidEmail);
+  await page.getByPlaceholder("Пароль").fill(invalidPassword);
+  await page.getByTestId("login-submit-btn").click();
+  await expect(page.locator("text=Вы ввели неправильно логин или пароль")).toBeVisible();
+});
